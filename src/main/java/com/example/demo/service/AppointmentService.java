b5697fb087044051650c4d089ccab9d43d575599
bala-amypo <bala@amypo.in>
2025-12-25 06:39:53 +0000

    User commit

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -4,8 +4,12 @@ import com.example.demo.dto.AppointmentDTO;
 import java.util.List;
 
 public interface AppointmentService {
-    AppointmentDTO createAppointment(Long visitorId, Long hostId, AppointmentDTO appointmentDTO);
+
+    AppointmentDTO createAppointment(Long visitorId, Long hostId, AppointmentDTO dto);
+
     AppointmentDTO getAppointment(Long id);
+
     List<AppointmentDTO> getAppointmentsForHost(Long hostId);
+
     List<AppointmentDTO> getAppointmentsForVisitor(Long visitorId);
 }
