2e7303215de441a911922ad1762ea09ff76975ae
bala-amypo <bala@amypo.in>
2025-12-26 05:27:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertNotificationRepository.java
+++ src/main/java/com/example/demo/repository/AlertNotificationRepository.java
@@ -1,28 +1,3 @@
-VisitorRepository.java
-
-
-HostRepository.java
-
-
-AppointmentRepository.java
-
-
-VisitLogRepository.java
-
-package com.example.demo.repository;
-
-import com.example.demo.entity.VisitLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
-
-    List<VisitLog> findByCheckOutTimeIsNull();
-}
-
-AlertNotification.java
-
 package com.example.demo.repository;
 
 import com.example.demo.entity.AlertNotification;
@@ -36,21 +11,6 @@ public interface AlertNotificationRepository extends JpaRepository<AlertNotifica
 }
 
 
-UserRepository.java
-
-package com.example.demo.repository;
-
-import com.example.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByUsername(String username);
-}
 
 
 
